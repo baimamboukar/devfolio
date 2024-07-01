42d44a45e0194fc587e7d41c71d116d10fcd3e7a
baimamboukar <baimamboukar@gmail.com>
2024-07-01 22:10:47 +0100

    init: Devfolio@baimamboukar

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<script setup>
+useHead({
+  titleTemplate: "%s · Baimam Boukar",
+});
+
+const route = useRoute();
+
+const serverMeta = {
+  ogImage: "/og-image.png",
+  twitterImage: "/og-image.png",
+  ogType: "website",
+  ogLocale: "en_US",
+  twitterCard: "summary",
+  twitterCreator: "@baimamboukar",
+};
+
+useSeoMeta({
+  title: () => route.meta.title,
+  description: () => route.meta.description,
+  ogTitle: () => `${route.meta.title} · Baimam Boukar`,
+  twitterTitle: () => `${route.meta.title} · Baimam Boukar`,
+  ogDescription: () => route.meta.description,
+  twitterDescription: () => route.meta.description,
+  ogUrl: () => `https://baimamboukar.dev${route.path}`,
+  ...serverMeta,
+});
+</script>
+
+<!-- Default Layout -->
+<template>
+  <main id="main-content">
+    <slot />
+  </main>
+</template>
---
+++ nuxt.config.js
@@ -0,0 +1,101 @@
+import tailwindTypography from "@tailwindcss/typography";
+
+export default defineNuxtConfig({
+  app: {
+    rootId: "app",
+    rootTag: "body",
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
+        { name: "author", content: "Baimam Boukar (@baimamboukar)" },
+        { name: "robots", content: "index, follow" },
+      ],
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest",
+        },
+      ],
+      script: [
+        {
+          type: "text/partytown",
+          src: "https://measure.baimamboukar.dev/minch-measure",
+          async: true,
+          "data-website-id": "1e0e9daa-a25b-4943-af4e-68c91b2d3c72",
+        },
+      ],
+    },
+    pageTransition: { name: "slide", mode: "out-in" },
+    layoutTransition: { name: "slide", mode: "out-in" },
+  },
+  components: [{ path: "~/components", pathPrefix: false }],
+  content: {
+    contentHead: false,
+    documentDriven: false,
+    highlight: {
+      theme: "github-dark",
+      preload: ["py"],
+    },
+    ignores: ["drafts", "template.md", "/drafts/", "\\.yml$"],
+    markdown: {
+      toc: { depth: 1, searchDepth: 2 },
+    },
+  },
+  modules: [
+    "@nuxt/content",
+    "@nuxtjs/partytown",
+    "@nuxtjs/turnstile",
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "nuxt-icon",
+  ],
+  routeRules: {
+    "/": { prerender: true },
+    "/**": { prerender: true },
+    "/bookmarks": { isr: 3600 },
+  },
+  runtimeConfig: {
+    // raindropToken: process.env.RAINDROP_TOKEN,
+    // raindropCollectionId: process.env.RAINDROP_COLLECTION_ID,
+    resendApiKey: process.env.RESEND_API_KEY,
+    resendFrom: process.env.RESEND_FROM,
+    resendTo: process.env.RESEND_TO,
+    // turnstile: { secretKey: process.env.TURNSTILE_SECRET_KEY },
+  },
+  tailwindcss: {
+    config: {
+      mode: "jit",
+      darkMode: "class",
+      content: [
+        "./app.vue",
+        "./components/**/*.vue",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+      ],
+      plugins: [tailwindTypography],
+    },
+  },
+  telemetry: false,
+  turnstile: {
+    siteKey: "0x4AAAAAAABlDwSHN3HVmFWH",
+  },
+});
