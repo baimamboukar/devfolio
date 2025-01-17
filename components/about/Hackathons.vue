c0745605034b38849cfbd4ca4d52a18f0d82be58
baimamboukar <baimamboukar@gmail.com>
2025-01-17 02:31:55 +0200

    chore: arrange about and home uis

--- components/about/Hackathons.vue
+++ components/about/Hackathons.vue
@@ -11,7 +11,7 @@
 <template>
   <section class="mb-12">
     <h2 class="text-2xl font-bold mb-8">Hackathons</h2>
-    <div class="space-y-8">
+    <div class="space-y-6">
       <div
         v-for="(hackathon, index) in hackathons"
         :key="hackathon.name"
@@ -22,7 +22,7 @@
           v-if="index !== hackathons.length - 1"
           class="absolute left-20 top-28 bottom-0 w-0.5 border-l-2 border-dashed border-gray-300 dark:border-gray-700"
         />
-        <UCard class="bg-gray-50 dark:bg-gray-900/50">
+        <UCard class="bg-gray-50 dark:bg-gray-900/50 cursor-pointer">
           <div class="relative">
             <nuxt-img
               :src="hackathon.icon"
@@ -35,22 +35,21 @@
             />
             <div class="ml-32 min-h-[160px]">
               <UBadge
-                color="green"
+                color="sky"
                 variant="subtle"
-                class="absolute top-4 right-4"
+                class="absolute top-0 right-4"
               >
                 {{ hackathon.date }}
               </UBadge>
-              <div class="space-y-3 pt-4">
+              <div class="space-y-1 pt-0">
                 <h3 class="text-xl font-semibold">{{ hackathon.name }}</h3>
                 <div class="space-y-1 text-gray-600 dark:text-gray-400">
-                  <p>Organized by {{ hackathon.organizer }}</p>
-                  <p>{{ hackathon.timeframe }}</p>
-                  <p>{{ hackathon.place }}</p>
+                  <p>Organized by {{ hackathon.by }}</p>
+                  <p class="text-blue-400">{{ hackathon.place }}</p>
                 </div>
               </div>
               <UButton
-                color="primary"
+                color="sky"
                 variant="ghost"
                 size="sm"
                 class="absolute bottom-4 right-4"
